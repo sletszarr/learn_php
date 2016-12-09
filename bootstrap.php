47275e2301de1ae5d26f298f9e112e3edda2184c
sletszarr <bnamora@aurasoft.co>
2016-12-09 14:44:32 +0700

    Refactoring PDO using config.php

--- bootstrap.php
+++ bootstrap.php
@@ -6,8 +6,9 @@
  * Time: 1:48 PM
  */
 
+$config = require "config.php";
 require 'database/Connection.php';
 require 'database/QueryBuilder.php';
 
 // get Query instance
-return new QueryBuilder(Connection::make());
+return new QueryBuilder(Connection::make($config['database']));
---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bnamora
+ * Date: 12/9/16
+ * Time: 2:34 PM
+ */
+
+return [
+    'database' => [
+        'name' => 'mytodo',
+        'username' => 'root',
+        'password' => 'janganliat2',
+        'connection' => 'mysql:host=127.0.0.1',
+        'options' => []
+    ]
+];
