dde73450be9ca2cfc1206b0e62d015666d222225
sletszarr <bnamora@aurasoft.co>
2016-12-15 21:11:35 +0700

    Introducing array_filter

---
+++ arrays/index.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DWP
+ * Date: 12/15/16
+ * Time: 7:02 PM
+ */
+
+class Post {
+
+    public $title;
+
+    public $published;
+
+    public function __construct($title, $published)
+    {
+        $this->title = $title;
+        $this->published = $published;
+    }
+
+}
+
+$posts = [
+    new Post("My first post", true),
+    new Post("My second post", true),
+    new Post("My third post", false),
+    new Post("My fourth post", true)
+];
+
+$publishedPost = array_filter($posts, function ($post) {
+    return $post->published;
+});
+
+$unpublishedPost = array_filter($posts, function ($post) {
+    return !$post->published;
+});
+
+var_dump($unpublishedPost);
+
+
